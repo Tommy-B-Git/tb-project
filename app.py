fd508c6f085e601cada71623fd270b51983e366b
Tom Brown <40210205@live.napier.ac.uk>
2016-11-17 13:22:53 +0000

    Added custom 404 route

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def basic():
 def premium():
   return "This will be the Premium signup page"
 
+#custom 404 Route
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('404.html'), 404
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
 
