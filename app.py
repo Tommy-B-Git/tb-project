a0aeb04294ec356195a18948bdf83977a6447366
Tom Brown <40210205@live.napier.ac.uk>
2016-11-17 13:12:00 +0000

    Added Basic and Premium routes

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def index():
   return render_template("base.html")
 
+@app.route("/basic")
+def basic():
+  return "This will be the basic signup page"
+
+@app.route("/premium")
+def premium():
+  return "This will be the Premium signup page"
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
 
