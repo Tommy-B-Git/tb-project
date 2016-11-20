f1effe18ded845784dffbbde5d2bcb54f9ff18ac
Tom Brown <40210205@live.napier.ac.uk>
2016-11-20 17:24:55 +0000

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, url_for, abort, request, render_template, json, g
-import sqlite3 
+from flask import Flask, redirect, url_for, abort, request, render_template, json, g, session, flash
+import sqlite3
+import models as dbHandler
 
 app = Flask(__name__)
 
@@ -11,29 +12,20 @@ def index():
 def basic():
   return render_template("signUp.html")
 
-@app.route("/premium")
-def premium():
-  return "This will be the Premium signup page"
-
 @app.route("/adduser", methods=['GET', 'POST'])
 def adduser():
   if request.method == 'POST':
-    try:
-      email = request.form['user_email']
-      password = request.form['user_password']
-         
-      with sql.connect("database.db") as con:
-        cur = con.cursor()
-        cur.execute("INSERT INTO users (user_email,user_password) VALUES (?,?)",(email,password))
-        con.commit()
-        msg = "Record successfully added"
-    except:
-      con.rollback()
-      msg = "error in insert operation"
-      
-    finally:
-      return render_template("createSee.html",msg = msg)
-      con.close()
+    email = request.form['user_email']
+    password = request.form['user_password']
+    dbHandler.insertUser(email, password)
+    user = dbHandler.retrieveUsers()
+    return render_template("createSee.html", user=user)
+  else:
+    return render_template("signUp.html")
+
+@app.route("/premium")
+def premium():
+  return "This will be the Premium signup page"
 
 @app.route("/users/new")
 def create_profile():
