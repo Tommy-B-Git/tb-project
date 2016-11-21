551828a3b459d321fde20076f583c90e336fa231
Tom Brown <40210205@live.napier.ac.uk>
2016-11-21 11:10:28 +0000

    New Schema & DB

--- var/schema.sql
+++ var/schema.sql
@@ -1,6 +1,5 @@
-drop table if exists users;
-create table users (
-  id integer primary key autoincrement,
-  email text unique not null,
-  password text not null
+DROP TABLE if EXISTS users;
+CREATE TABLE users (
+email text UNIQUE,
+password text
 );
