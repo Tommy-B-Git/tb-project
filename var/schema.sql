348aad7dbf7f096e29fd9791d4dea181a44bc911
Tom Brown <40210205@live.napier.ac.uk>
2016-11-24 10:47:59 +0000

    Update DB/Schema

--- var/schema.sql
+++ var/schema.sql
@@ -3,3 +3,13 @@ drop table if exists users;
   email text not null,
   password text not null
 );
+
+drop table if exists profiles;
+  create table profiles (
+  username text not null,
+  location text not null,
+  bio text not null,
+  gender text not null,
+  prof_img text
+);
+
