b5b39c97f27584c18e159404b06f8dadf80b28c0
Tom Brown <40210205@live.napier.ac.uk>
2016-11-26 19:53:18 +0000

    Added update profile route

--- app.py
+++ app.py
@@ -108,13 +108,13 @@ def login():
   if request.method == 'POST':
     email = request.form['user_email']
     password = request.form['user_password']
-    if validate(email, password):
+    if validate(email, password) or validate_prem(email, password):
       session['logged_in'] = True
       success = 'You have succesfully logged in'
       return redirect(url_for('my_profile', email=email))
     else:
       error = "Wrong details, try again!"
-  return render_template("login.html", error=error)
+  return render_template("login.html")
 
 
 @app.route("/basic")
@@ -126,24 +126,24 @@ def basic():
 @app.route('/adduser',methods = ['POST', 'GET'])
 def adduser():
   if request.method == 'POST':
+    username = request.form['username']
     email = request.form['user_email']
     password = request.form['user_password']
     # Try to hash and salt password
     #valid_pwhash = bcrypt.hashpw(password, bcrypt.gensalt())
 
     db = get_db()
-    db.cursor().execute("INSERT INTO users (email,password) VALUES (?,?)", (email,password) )
+    db.cursor().execute("INSERT INTO users (email,password,username) VALUES(?,?,?)", (email,password,username) )
     db.commit()
-    msg = "Account created, now create your profile"
-    return redirect(url_for('login'))
+    return redirect(url_for('login',username=username))
   else:
     return render_template("signUp.html")
 
 
 # CREATE PROFILE PAGE
-@app.route("/user/new",methods = ['GET', 'POST'])
+@app.route("/profile/create",methods = ['GET', 'POST'])
 def create_profile():
-  error = None
+  # error = None
   if request.method == 'POST':
     email = request.form['email']
     username = request.form['username']
@@ -157,6 +157,7 @@ def create_profile():
     f = request.files['datafile']
     new_file = f.filename
     if new_file == "":
+      # Make page for line below if time!!
       return "Please select a file to upload"
     else:
       f.save('static/img/' + new_file)
@@ -165,18 +166,44 @@ def create_profile():
     db.cursor().execute("INSERT INTO profiles(email,username,location,bio,gender,prof_img) VALUES (?,?,?,?,?,?)",(email,username,location,bio,gender,new_file))
     db.commit()
     msg = "Profile Created!"
-    return redirect(url_for('my_profile'))
+    return redirect(url_for('my_profile', email=email))
   else:
     return render_template('newProfile.html')
 
+# UPDATE PROFILE ROUTE #
+@app.route("/profile/update", methods = ['GET', 'POST'])
+def update_profile():
+  if request.method == 'POST':
+    email = request.form['email']
+    username = request.form['username']
+    location = request.form['location']
+    bio = request.form['bio']
+    gender = request.form['gender']
+    prof_img = request.form['datafile']
+
+    f = request.files['datafile']
+    new_file = f.filename
+    if new_file == "":
+      # Make page for line below if time!!
+      return "Please select a file to upload"
+    else:
+      f.save('static/img/' + new_file)
+
+    db = get_db
+    db.cursor().execute("UPDATE profiles SET email = ?, username = ?,location = ?, bio = ?, gender = ?, prof_img = ?) WHERE email = ?" ,(email,username,location,bio,gender,prof_img))
+    #cur.execute("UPDATE profiles SET email=?,username=?,location=?,bio=?,gender=?,prof_img=? WHERE email=?",(email,username,location,bio,gender,prof_img))
+    db.commit()
+    return redirect(url_for('my_profile', email=email))
+  else:
+    return render_template('update.html')
 
 @app.route("/members/view")
 def view_members():
-  conn = sqlite3.connect('var/database.db')
-  with conn:
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM profiles')
-    rows = cur.fetchall()
+ # conn = sqlite3.connect('var/database.db')
+ # with conn:
+  #  cur = conn.cursor()
+  #  cur.execute('SELECT * FROM profiles')
+  #  rows = cur.fetchall()
     #for row in rows:
       #dbEmail = row[0]
       #dbPass = row[1]
@@ -190,9 +217,43 @@ def premium():
   return render_template("premium.html")
 
 
+@app.route("/addpremium", methods = ['GET', 'POST'])
+def add_premium():
+  if request.method == 'POST':
+    username = request.form['username']
+    password =  request.form['password']
+    email = request.form['email']
+    cc_number = request.form['cc_number']
+    sec_code = request.form['sec_code']
+
+    db = get_db()
+    db.cursor().execute("INSERT INTO premium (username,password,email,cc_number,sec_code) VALUES (?,?,?,?,?)",(username,password,email,cc_number,sec_code) )
+    db.commit()
+    return redirect(url_for('login'))
+  else:
+    return render_template("premium.html")
+
+# Validate login                                                                                            
+def validate_prem(email, password):
+  conn = sqlite3.connect('var/database.db')
+  with conn:
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM premium WHERE email=(?)', (email,))
+    rows = cur.fetchall()
+    for row in rows:
+      dbEmail = row[0]
+      dbPass = row[1]
+      #print(dbEmail, email, dbPass, password)                                                              
+      if  dbEmail == email and dbPass == password:
+      # CHECK HASHED PW                                                                                     
+      #if (email == dbEmail and password == bcrypt.hashpw(password.encode('utf-8'), password)):             
+        return True
+      else:
+        return False
+
 #custom 404 Route
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found():
   return render_template('404.html'), 404
 
 if __name__ == "__main__":
