b8391887d08cf34d88be1e8258b9f6f5d53f50f9
Tom Brown <40210205@live.napier.ac.uk>
2016-11-28 21:49:12 +0000

    No changes

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,12 @@
-var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+function goNewWin() {
+
+//***Get what is below onto one line***
+
+window.open("backbuttonnewpage.html",'TheNewpop','toolbar=1,location=1,directories=1,status=1,menubar=1,scrollbars=1,resizable=1'); 
+
+//***Get what is above onto one line*** 
+
+self.close()
+
 }
+
